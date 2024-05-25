cde5443fcc72d5a7ab018de14445a049e9de75c9
naveen2314 <naveen2314@yahoo.com>
2024-05-25 12:27:56 +0400

    added backend file

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-eks-s3-backend" #"<your_bucket_name>"
-    key            = "terraform/state"
-    region         = "us-west-2"            # Update with your desired region
+    bucket         = "my-iac-s3-backend" #"<your_bucket_name>"
+    key            = "terraform/tf.state"
+    region         = "us-west-1"            # Update with your desired region
     dynamodb_table = "terraform-state-lock" # Optional: Use DynamoDB for state locking
   }
 }
