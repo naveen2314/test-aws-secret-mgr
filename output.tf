36834c5bca6ba83b2b176468b5622dc0e3063bd3
naveen2314 <naveen2314@yahoo.com>
2024-05-25 12:17:03 +0400

    Commented Output file

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "secrets_arns" {
-  description = "The ARNs of the created secrets"
-  value       = { for k, v in module.secrets : k => v.secret_arn }
-}
+# output "secrets_arns" {
+#   description = "The ARNs of the created secrets"
+#   value       = { for k, v in module.secrets : k => v.secret_arn }
+# }
 
-output "secrets_names" {
-  description = "The names of the created secrets"
-  value       = { for k, v in module.secrets : k => v.secret_name }
-}
+# output "secrets_names" {
+#   description = "The names of the created secrets"
+#   value       = { for k, v in module.secrets : k => v.secret_name }
+# }
